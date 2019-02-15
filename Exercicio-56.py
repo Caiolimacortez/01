7e6d193aa1d234dc53f0a10a1b9591659c0b4fcd
caio <caiolimacortez@gmail.com>
2019-02-15 17:31:05 -0200

    Melhorias-02

--- Exercicio-56.py
+++ Exercicio-56.py
@@ -1,18 +1,25 @@
 from time import sleep
 import os
 
-Mulheres = 0
-Idade_1 = 0
-Idade_2 = 0
-Idade_3 = 0
-Idade_4 = 0
-
-
+Mulheres: int = 0
+IdadeTotalMedia = 0
+Homem = 0
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 print('-='* 20)
 Nome1 = str(input('Digite o Seu Nome: '))
 
+def AntiErro1():
+  Idade1 = input('Digite a Sua Idade: ')
 
-Idade1 = int(input('Digite a Sua Idade: '))
+  try:
+    return int(Idade1)
+  except Exception as err:
+    print('Digite Somente Numeros Inteiros! Ex = [1] [2]... ')
+  return AntiErro1()
+
+Idade1 = (AntiErro1())
+
+#print("Idade1 [{}]".format(Idade1))
 
 while True:
   Sexo1 = str(input('Digite o Seu Sexo [M/F]: '))
@@ -28,10 +35,28 @@ while True:
   else:
     break
 
-print('-='* 20)
+if Sexo1 == ('M'):
+  Maisvelho = Idade1
+  Homem = Nome1
 
+elif Idade1 <= 20:
+  Mulheres += 1
+
+print('-='* 20)
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 Nome2 = str(input('Digite o Seu Nome: '))
-Idade2 = int(input('Digite a Sua Idade: '))
+
+def AntiErro2():
+  Idade2 = input('Digite a Sua Idade: ')
+
+  try:
+    return int(Idade2)
+  except Exception as err:
+    print('Digite Somente Numeros Inteiros! Ex = [1] [2]... ')
+  return AntiErro2()
+
+Idade2 = (AntiErro2())
+
 while True:
   Sexo2 = str(input('Digite o Seu Sexo [M/F]: '))
 
@@ -39,7 +64,7 @@ while True:
   Sexo2 = Sexo2.strip()
 
   if Sexo2 != ('M') and  Sexo2 != ('F'):
-    print('\nOpção Inválida Voçê Digitou = {}'.format(Sexo1))
+    print('\nOpção Inválida Voçê Digitou = {}'.format(Sexo2))
     print('Digite - M - Para Masculino é - F - Para Feminino')
     sleep(0.3)
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -47,9 +72,29 @@ while True:
   else:
     break
 
+if Sexo2 == ('M'):
+  if Idade2 >= Idade1:
+    Maisvelho = Idade1
+    Homem = Nome2
+
+elif Idade2 <= 20:
+  Mulheres += 1
+
 print('-='* 20)
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 Nome3 = str(input('Digite o Seu Nome: '))
-Idade3 = int(input('Digite a Sua Idade: '))
+
+def AntiErro3():
+  Idade3 = input('Digite a Sua Idade: ')
+
+  try:
+    return int(Idade3)
+  except Exception as err:
+    print('Digite Somente Numeros Inteiros! Ex = [1] [2]... ')
+  return AntiErro3()
+
+Idade3 = (AntiErro3())
+
 while True:
   Sexo3 = str(input('Digite o Seu Sexo [M/F]: '))
 
@@ -57,7 +102,7 @@ while True:
   Sexo3 = Sexo1.strip()
 
   if Sexo3 != ('M') and  Sexo3 != ('F'):
-      print('\nOpção Inválida Voçê Digitou = {}'.format(Sexo1))
+      print('\nOpção Inválida Voçê Digitou = {}'.format(Sexo3))
       print('Digite - M - Para Masculino é - F - Para Feminino')
       sleep(0.3)
       os.system('cls' if os.name == 'nt' else 'clear')
@@ -65,9 +110,28 @@ while True:
   else:
     break
 
+if Sexo3 == ('M'):
+  Maisvelho = Idade3
+  Homem = Nome3
+
+elif Idade3 <= 20:
+  Mulheres += 1
+
 print('-='* 20)
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 Nome4 = str(input('Digite o Seu Nome: '))
-Idade4 = int(input('Digite a Sua Idade: '))
+
+def AntiErro4():
+  Idade4 = input('Digite a Sua Idade: ')
+
+  try:
+    return int(Idade4)
+  except Exception as err:
+    print('Digite Somente Numeros Inteiros! Ex = [1] [2]... ')
+  return AntiErro4()
+
+Idade4 = (AntiErro4())
+
 while True:
   Sexo4 = str(input('Digite o Seu Sexo [M/F]: '))
 
@@ -75,7 +139,7 @@ while True:
   Sexo4 = Sexo1.strip()
 
   if Sexo4 != ('M') and  Sexo4 != ('F'):
-      print('\nOpção Inválida Voçê Digitou = {}'.format(Sexo1))
+      print('\nOpção Inválida Voçê Digitou = {}'.format(Sexo4))
       print('Digite - M - Para Masculino é - F - Para Feminino')
       sleep(0.3)
       os.system('cls' if os.name == 'nt' else 'clear')
@@ -83,74 +147,18 @@ while True:
   else:
     break
 
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-if  Sexo1 == ('F'):
-  Idade_1 = 1
-  if Idade1 <= 20:
-    Mulheres = +1
+if Sexo4 == ('M') and Maisvelho <= Idade4:
+  Maisvelho = Idade4
+  Homem = Nome4
 
-if Sexo2 == ('F'):
-  Idade_2 = 1
-  if Idade2 <= 20:
-    Mulheres = +1
-
-if Sexo3 == ('F'):
-  Idade_3 = 1
-  if Idade3 <= 20:
-    Mulheres +1
-
-if Sexo4 == ('F'):
-  Idade_4 = 1
-  if Idade4 <= 20:
-    Mulheres +1
+elif Idade4 <= 20:
+  Mulheres += 1
 
 #-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+IdadeTotalMedia = (Idade1 + Idade2 + Idade3 + Idade4) /4
 
-Idade = (Idade1 + Idade2 + Idade3 + Idade4) /4
-
-if  Idade_1 == 1:
-  if Idade2 > Idade3 and Idade4:
-    Homem = Nome2
-    IdadeH = Idade2
-
-if  Idade_2 == 1:
-  if Idade1 > Idade3 and Idade4:
-    Homem = Nome1
-    IdadeH = Idade1
-
-if  Idade_3 == 1:
-  if Idade4 > Idade2 and Idade1:
-    Homem = Nome4
-    IdadeH = Idade4
-
-if  Idade_4 == 1:
-  if Idade3 > Idade1 and Idade2:
-    Homem = Nome3
-    IdadeH = Idade3
-
-else:
-  if Sexo1 == ('M'):
-    if Idade1 > Idade2 and Idade3 and Idade4:
-      Homem = Nome1
-      IdadeH = Idade1
-
-  if Sexo2 == ('M'):
-    if Idade2 > Idade2 and Idade3 and Idade4:
-      Homem = Nome2
-      IdadeH = Idade2
-
-  if Sexo3 == ('M'):
-    if Idade3 > Idade1 and Idade2 and Idade4:
-      Homem = Nome3
-      IdadeH = Idade3
-
-  if Sexo4 == ('M'):
-    if Idade4 > Idade1 and Idade2 and Idade3:
-      Homem = Nome4
-      IdadeH = Idade4
-
-#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-
-print('\nHá {} Com 20 Anos De Idade'.format(Mulheres))
-print('\nA Idade Media do Grupo é de {:.0f} Anos'.format(Idade))
-print('\nO nome do Homen Mais velho do grupo é {} com {] Anos'.format(Homem, IdadeH))
+if Mulheres != 0:
+  print('\nHá {} Mulheres Com 20 Anos De Idade.'.format(Mulheres))
+print('\nA Idade Media do Grupo é de {:.0f} Anos.'.format(IdadeTotalMedia))
+if Homem != 0:
+  print('\nO Homem Mais Velho é {} Com {} Anos De Idade.'.format(Homem,Maisvelho))
